ffdc1d27f359b55fe93708aa4e0b3a13807485de
basit <nadeemzahoor00690@gmail.com>
2021-02-21 18:07:17 +0500

    implemented

--- bootcamp.js
+++ bootcamp.js
@@ -1,8 +1,16 @@
 function isEven(n) {
+  if(n%2 == 0)
+  return true;
+  else
+  return false;
   // Write your code here that will return true if n is an even number otherwise false
 }
 
 function isOdd(n) {
+  if(n%2 != 0)
+  return true;
+  else 
+  false;
   // Write your code here that will return true if n is an odd number otherwise false
 }
 
